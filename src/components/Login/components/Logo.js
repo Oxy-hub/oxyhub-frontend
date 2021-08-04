e3f149a106b49235d4dd87dfbd497fdde69c750c
saby22 <sabyasachi.tffs@gmail.com>
2021-08-04 21:47:17 +0530

    Placed Logo

---
+++ src/components/Login/components/Logo.js
@@ -0,0 +1,11 @@
+import {ReactComponent as OxyhubLogo} from '../../../assets/oxyhub_logo.svg';
+import {LogoContainer, SubHeading, HeaderContainer} from './logo.styled';
+const Logo = () => {
+	return (
+		<LogoContainer>
+			<OxyhubLogo />
+		</LogoContainer>
+	);
+};
+
+export default Logo;
