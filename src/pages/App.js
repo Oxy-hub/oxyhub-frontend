8106912a477f270108edd5f3608ac7d0c22c4459
saby22 <sabyasachi.tffs@gmail.com>
2021-09-15 18:52:43 +0530

    Removed unecessary files from pages firectory

--- src/pages/App.js
+++
@@ -1,3 +0,0 @@
-const App = () => <div>Welcome to the Application !!</div>;
-
-export default App;
--- src/pages/__tests__/LoginPage.spec.js
+++
@@ -1,7 +0,0 @@
-import snapshot from 'react-test-renderer';
-import LoginPage from '../LoginPage';
-
-test('Snapshot test for the login page', () => {
-  const componentSnapshot = snapshot.create(<LoginPage />).toJSON();
-  expect(componentSnapshot).toMatchSnapshot();
-});
