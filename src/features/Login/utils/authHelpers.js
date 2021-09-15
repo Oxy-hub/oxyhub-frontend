f6ad40bdb4f3e3328f77675e4a9c505e41c316cf
saby22 <sabyasachi.tffs@gmail.com>
2021-09-15 20:30:12 +0530

    Added spaces for greater readability

--- src/features/Login/utils/authHelpers.js
+++ src/features/Login/utils/authHelpers.js
@@ -8,12 +8,15 @@ export const googleAuthHelper = () => {
 
 export const githubAuthHelper = () => {
   // Logic for what happens when Login with Github button is clicked
+  const state = cryptoRandomString({ length: 10, type: 'url-safe' });
+
   const CLIENT_ID = config.github.clientId;
   const SCOPE = config.github.scope;
   const REDIRECT_URL = config.github.redirectUrl;
-  const state = cryptoRandomString({ length: 10, type: 'url-safe' });
+
   localStorage.setItem('isLoggingIn', true);
   localStorage.setItem('state', state);
+
   window.location.assign(
     `https://github.com/login/oauth/authorize?scope=${SCOPE}&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&state=${state}`
   );
