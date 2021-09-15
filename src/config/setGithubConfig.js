b71cd6abd909dc697f60a018b44cac86cbf7de52
saby22 <sabyasachi.tffs@gmail.com>
2021-09-15 19:31:29 +0530

    Added github config initialization function based on environment

---
+++ src/config/setGithubConfig.js
@@ -0,0 +1,26 @@
+const setGithubConfig = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return {
+        clientId: process.env.REACT_APP_GITHUB_CLIENT_ID_DEV,
+        scope: process.env.REACT_APP_GITHUB_SCOPE_DEV,
+        redirectUrl: process.env.REACT_APP_GITHUB_REDIRECT_URL_DEV
+      };
+
+    case 'test':
+      return {
+        clientId: process.env.REACT_APP_GITHUB_CLIENT_ID_TEST,
+        scope: process.env.REACT_APP_GITHUB_SCOPE_TEST,
+        redirectUrl: process.env.REACT_APP_GITHUB_REDIRECT_URL_TEST
+      };
+
+    default:
+      return {
+        clientId: process.env.REACT_APP_GITHUB_CLIENT_ID_DEV,
+        scope: process.env.REACT_APP_GITHUB_SCOPE_DEV,
+        redirectUrl: process.env.REACT_APP_GITHUB_REDIRECT_URL_DEV
+      };
+  }
+};
+
+export default setGithubConfig;
