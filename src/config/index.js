5ac117178e44ed5330882e7f85593a131fc9b601
saby22 <sabyasachi.tffs@gmail.com>
2021-09-15 19:07:30 +0530

    Functionality to add custom envrionment variables for different envrironments

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,17 @@
+const setBaseUrl = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return process.env.REACT_APP_API_BASE_URL_PROD;
+
+    case 'test':
+      return process.env.REACT_APP_API_BASE_URL_TEST;
+
+    default:
+      return process.env.REACT_APP_API_BASE_URL_DEV;
+  }
+};
+
 export default {
-  baseURL: process.env.REACT_APP_API_BASE_URL
+  baseURL: setBaseUrl(),
+  enableMock: process.env.REACT_APP_MOCK_API
 };
