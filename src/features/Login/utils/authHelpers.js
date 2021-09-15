638b3e2e7674c070fdd2f1a5891a32c477905e08
saby22 <sabyasachi.tffs@gmail.com>
2021-09-15 18:30:44 +0530

    Changed window.location.href to window.location.assign

--- src/features/Login/utils/authHelpers.js
+++ src/features/Login/utils/authHelpers.js
@@ -13,5 +13,7 @@ export const githubAuthHelper = () => {
   const state = cryptoRandomString({ length: 10, type: 'url-safe' });
   localStorage.setItem('isLoggingIn', true);
   localStorage.setItem('state', state);
-  window.location.href = `https://github.com/login/oauth/authorize?scope=${SCOPE}&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&state=${state}`;
+  window.location.assign(
+    `https://github.com/login/oauth/authorize?scope=${SCOPE}&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&state=${state}`
+  );
 };
