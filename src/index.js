dc80ee991c8fb863268ce76c4551a9e5e07038ac
saby22 <sabyasachi.tffs@gmail.com>
2021-09-15 19:08:11 +0530

    Functionality to read enableMock boolean from config file and not directly from process.env

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
+import config from './config';
 import worker from './mocks/browser';
 
 const prepareMSW = () => {
-  if (process.env.REACT_APP_MOCK_API === 'true') {
+  if (config.enableMock === 'true') {
     return worker.start();
   }
   return Promise.resolve();
