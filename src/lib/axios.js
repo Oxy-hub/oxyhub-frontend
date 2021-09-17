47ac2498e2ed4ebdb8b8d013de5bd7e3c783c58c
saby22 <sabyasachi.tffs@gmail.com>
2021-09-17 20:04:26 +0530

    Included withCredentials true in the axios config

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import config from '../config';
 
 const Axios = axios.create({
-  baseURL: config.baseURL
+  baseURL: config.baseURL,
+  withCredentials: true
 });
 
 export default Axios;
