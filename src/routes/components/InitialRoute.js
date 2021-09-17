edca7beb59f1eee2bdee67e3ac4bdf99996afd80
saby22 <sabyasachi.tffs@gmail.com>
2021-09-17 21:30:31 +0530

    Removed layout from initial route

--- src/routes/components/InitialRoute.js
+++ src/routes/components/InitialRoute.js
@@ -1,22 +1,19 @@
 import PropType from 'prop-types';
 import { Redirect, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import Layout from '../../components/common/Layout';
 
 const InitialRoute = ({ component: Component, ...rest }) => {
   const isInitial = useSelector(state => state.initialUser.isInitial);
 
   return (
-    <Layout>
-      <Route
+    <Route
+      // eslint-disable-next-line
+      {...rest}
+      render={props =>
         // eslint-disable-next-line
-        {...rest}
-        render={props =>
-          // eslint-disable-next-line
-          isInitial ? <Component {...props} /> : <Redirect to="/" />
-        }
-      />
-    </Layout>
+        isInitial ? <Component {...props} /> : <Redirect to="/" />
+      }
+    />
   );
 };
 
