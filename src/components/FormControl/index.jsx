4381b6a29921bf9e6f17f41377386211bb5a02e2
saby22 <sabyasachi.tffs@gmail.com>
2021-09-19 02:12:52 +0530

    Included Submit control option in switch case

--- src/components/FormControl/index.jsx
+++ src/components/FormControl/index.jsx
@@ -1,10 +1,14 @@
 import Proptypes from 'prop-types';
 import Input from './Input';
+import Submit from './Submit';
 
 const FormControl = ({ control, ...rest }) => {
   switch (control) {
     // case 'input':
     //   return <Input {...rest} />;
+    case 'submit':
+      // eslint-disable-next-line
+      return <Submit {...rest} />;
     default:
       // eslint-disable-next-line
       return <Input {...rest} />;
