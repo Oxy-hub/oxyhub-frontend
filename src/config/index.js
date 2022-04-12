dafd555d4d217359cd691e8196a17e7b49058a85
saby22 <sabyasachi.tffs@gmail.com>
2022-04-12 08:18:57 +0530

    Updated config

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,15 @@
-import setBaseUrl from './setBaseUrl';
-import setGithubConfig from './setGithubConfig';
-
-export default {
-  baseURL: setBaseUrl(),
-  github: setGithubConfig(),
+module.exports = {
+  github: {
+    clientId: process.env.REACT_APP_GITHUB_CLIENT_ID,
+    scope: process.env.REACT_APP_GITHUB_SCOPE,
+    redirectUrl: process.env.REACT_APP_GITHUB_REDIRECT_URL
+  },
   google: {
     clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
     scope: process.env.REACT_APP_GOOGLE_SCOPE,
     redirectUrl: process.env.REACT_APP_GOOGLE_REDIRECT_URL
   },
+  apiBaseUrl: process.env.REACT_APP_API_BASE_URL,
+  apiPrefix: process.env.REACT_APP_API_PREFIX,
   enableMock: process.env.REACT_APP_MOCK_API
 };
