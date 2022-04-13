428e17d2e379b90f266c33a2492c7516b6e47f25
ishika1403 <ishikamukherjee14@gmail.com>
2022-04-13 21:13:21 +0530

    Completed both user details cards

--- src/features/Profile/components/UserProfile.jsx
+++ src/features/Profile/components/UserProfile.jsx
@@ -5,6 +5,7 @@ import dummyAvatar from '../../../assets/dummyAvatar.jpg';
 
 const UserProfile = () => {
   console.log('');
+  const name = ['First Name', 'Middle Name', 'Last Name'];
   return (
     <>
       <Box
@@ -20,62 +21,89 @@ const UserProfile = () => {
       </Box>
 
       <Center>
-        <Box
-          bg="white"
-          w="50%"
-          boxShadow="xl"
-          mt="10"
-          borderRadius="20"
-          sx={{ maxWidth: '1000px', minWidth: '768px' }}
-        >
-          {/* Account details Header Section  */}
+        <Flex direction="column">
+          {/* This box only contains account details of the concerned user */}
           <Box
-            width="100%"
-            h="96px"
-            borderTopRadius="20"
-            bgGradient={`linear(to-r, ${pallete.primary}, ${pallete.secondary})`}
-            position="relative"
+            bg="white"
+            w="50%"
+            boxShadow="xl"
+            mt={10}
+            mb={12}
+            borderRadius="xl"
+            sx={{ maxWidth: '1000px', minWidth: '768px' }}
           >
-            <Center position="absolute" left="11%" top="25%">
-              <Flex width="100%" align="center" justify="center">
-                <Avatar
-                  size="xl"
-                  name="Dummy User"
-                  src={`${dummyAvatar}`}
-                  sx={{ border: '3px solid #00BB8C' }}
-                />
-                <Heading as="h3" size="lg" ml="7" color="white">
-                  Hi, Ishika
-                </Heading>
+            {/* Account details Header Section  */}
+            <Box
+              width="100%"
+              h="96px"
+              borderTopRadius="xl"
+              bgGradient={`linear(to-r, ${pallete.primary}, ${pallete.secondary})`}
+              position="relative"
+            >
+              <Center position="absolute" left="11%" top="25%">
+                <Flex width="100%" align="center" justify="center">
+                  <Avatar
+                    size="xl"
+                    name="Dummy User"
+                    src={`${dummyAvatar}`}
+                    sx={{ border: '3px solid #00BB8C' }}
+                  />
+                  <Heading as="h3" size="lg" ml={7} color="white">
+                    Hi, Ishika
+                  </Heading>
+                </Flex>
+              </Center>
+            </Box>
+
+            {/* Account Details */}
+            <Box p="20" py="10">
+              <Heading as="h4" size="md" mb={5} color={`${pallete.fontBlack}`}>
+                Account Details
+              </Heading>
+
+              <Flex width="100%" align="center" mb={4}>
+                <Text fontSize="md" width="30%">
+                  Email
+                </Text>
+                <Text fontSize="lg">testuser@email.com</Text>
               </Flex>
-            </Center>
+
+              <Flex width="100%" align="center">
+                <Text fontSize="md" width="30%">
+                  Last login
+                </Text>
+                <Text fontSize="lg">5th January,2022</Text>
+              </Flex>
+            </Box>
           </Box>
 
-          {/* Account Details */}
-          <Box p="20" py="10">
-            <Heading as="h4" size="md" mb="5" color={`${pallete.fontBlack}`}>
-              Account Details
+          {/* This box contains other personal details of the user */}
+          <Box
+            bg="white"
+            w="50%"
+            boxShadow="xl"
+            mb={16}
+            borderRadius="xl"
+            sx={{ maxWidth: '1000px', minWidth: '768px' }}
+            p="20"
+            py="10"
+          >
+            <Heading as="h4" size="md" mb={5} color={`${pallete.fontBlack}`}>
+              Personal Details
             </Heading>
-
-            <Flex width="100%" align="center" mb="5">
-              <Text fontSize="md" width="30%">
-                Email
-              </Text>
-              <Text fontSize="lg">testuser@email.com</Text>
-            </Flex>
-
-            <Flex width="100%" align="center">
-              <Text fontSize="md" width="30%">
-                Last seen on
-              </Text>
-              <Text fontSize="lg">5th January,2022</Text>
-            </Flex>
+            {name.map(n => (
+              <Flex width="100%" align="center" mb={3}>
+                <Text fontSize="md" width="30%">
+                  {n}
+                </Text>
+                <Text fontSize="lg">Ishika Mukherjee</Text>
+              </Flex>
+            ))}
           </Box>
-        </Box>
+        </Flex>
       </Center>
     </>
   );
 };
 
 export default UserProfile;
-// border="1px" borderColor="black"
