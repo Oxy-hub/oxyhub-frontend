3309595a6800fa53e6b16f43bd87a4d8f09ba470
saby22 <sabyasachi.tffs@gmail.com>
2022-05-08 17:23:58 +0530

    Brought back theme provider for styled components to maintain backwards compatibility

--- src/providers/index.jsx
+++ src/providers/index.jsx
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ThemeProvider } from 'styled-components';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
@@ -8,6 +9,7 @@ import PropType from 'prop-types';
 
 import reduxStore from '../store';
 import chakraTheme from '../theme';
+import pallete from '../utils/pallete';
 import queryClient from '../lib/react-query';
 
 const Providers = ({ children }) => (
@@ -15,9 +17,11 @@ const Providers = ({ children }) => (
     <Provider store={reduxStore}>
       <QueryClientProvider client={queryClient}>
         <ChakraProvider theme={chakraTheme}>
-          {/* Implement React Helmet Here... */}
-          {children}
-          <ReactQueryDevtools initialIsOpen={false} />
+          <ThemeProvider theme={pallete}>
+            {/* Implement React Helmet Here... */}
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+          </ThemeProvider>
         </ChakraProvider>
       </QueryClientProvider>
     </Provider>
