c463cbb95dbb7fc4d20e83c3e3e7bf5bf3e70c98
saby22 <sabyasachi.tffs@gmail.com>
2022-05-08 17:16:04 +0530

    Removed webfontloader and implemented fonts according to Chakra UI docs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-// import { useEffect } from 'react';
-// import WebFont from 'webfontloader';
+import '@fontsource/poppins';
 import AppRoutes from './routes';
 import Providers from './providers';
 
 const App = () => (
-  // Web Font Loader
-  // useEffect(() => {
-  //   WebFont.load({
-  //     google: {
-  //       families: ['Poppins:400,500,600,700']
-  //     }
-  //   });
-  // }, []);
-
   <Providers>
     <AppRoutes />
   </Providers>
 );
+
 export default App;
