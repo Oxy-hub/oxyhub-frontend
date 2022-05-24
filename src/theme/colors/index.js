c4a4bc51b60caf76a8602913d981ee64f25ebb08
saby22 <sabyasachi.tffs@gmail.com>
2022-05-25 01:41:39 +0530

    Introduced brand colors and it's tints

---
+++ src/theme/colors/index.js
@@ -0,0 +1,53 @@
+export default {
+  colors: {
+    primary: {
+      700: '#FF725E', // Brand Primary Color
+      600: '#FF8675',
+      500: '#FF9A8C',
+      200: '#FFD7D1',
+      100: '#FFEBE8'
+    },
+    secondary: {
+      700: '#006096', // Brand Secondary Color
+      600: '#1BCFCF',
+      500: '#34E4E4',
+      200: '#AEF4F4',
+      100: '#D6FAFA'
+    },
+    success: {
+      700: '#2F6846', // Brand Success Color
+      600: '#328048',
+      500: '#5CB176',
+      200: '#C6F0C2',
+      100: '#EAFBE7'
+    },
+    warning: {
+      700: '#BE5D01', // Brand Warning Color
+      600: '#D9822F',
+      500: '#F29D41',
+      200: '#FAE7B9',
+      100: '#FDF4DC'
+    },
+    danger: {
+      700: '#7DE1F1F', // Brand Danger Color
+      600: '#E43E3E',
+      500: '#E95E5E',
+      200: '#F6BFBF',
+      100: '#FBDFDF'
+    },
+    neutral: {
+      1000: '#181826',
+      900: '#212134',
+      800: '#32324D',
+      700: '#4A4A6A',
+      600: '#666687',
+      500: '#8E8EA9',
+      400: '#A5A5BA',
+      300: '#C0C0CF',
+      200: '#DCDCE4',
+      150: '#EAEAEF',
+      100: '#F6F6F9',
+      0: '#FFFFFF'
+    }
+  }
+};
