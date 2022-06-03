74bb5a0e07497b8fb47e0257f438b68042eaf703
ishika1403 <ishikamukherjee14@gmail.com>
2022-06-04 02:53:48 +0530

    Implemented UI blueprint for Parlour details

--- src/features/ParlourDetails/components/index.jsx
+++ src/features/ParlourDetails/components/index.jsx
@@ -1,23 +1,35 @@
-import { Box, SimpleGrid, Image, Heading, Text } from '@chakra-ui/react';
+import {
+  Box,
+  SimpleGrid,
+  Image,
+  Heading,
+  Text,
+  HStack,
+  Button,
+  Divider,
+  Spacer,
+  Flex
+} from '@chakra-ui/react';
 import parlourImg from '../../../assets/parlourImg.png';
 
 const ParlourDetails = () => (
-  <Box maxW="1100px" w="100%" m="0 auto" border="2px solid black">
+  <Box maxW="1100px" w="100%" m="0 auto" mt={20}>
     <SimpleGrid
       columns={2}
       spacing={4}
       templateColumns="5fr 7fr"
       alignItems="center"
+      mb={13.5}
     >
       <Box>
         <Image
           src={parlourImg}
           objectFit="stretch"
-          border="2px solid red"
+          // border="2px solid red"
           m="0 auto"
         />
       </Box>
-      <Box border="2px solid red">
+      <Box>
         <Heading textStyle="alpha" as="h1" mb={10}>
           The Best Parlour
         </Heading>
@@ -47,12 +59,28 @@ const ParlourDetails = () => (
         </Box>
 
         <Box>
-          <Text textStyle="omegaSemibold" as="p">
+          <Text textStyle="omegaSemibold" as="p" mb={4}>
             Variants
           </Text>
+          <HStack spacing={6}>
+            <Button borderColor="danger.700">A</Button>
+            <Button borderColor="danger.700">B</Button>
+            <Button borderColor="danger.700">C</Button>
+            <Button borderColor="danger.700">D</Button>
+          </HStack>
         </Box>
       </Box>
     </SimpleGrid>
+
+    <Divider />
+
+    <Flex mt={12.5}>
+      <Box>Prices will be calculated in the next step ...</Box>
+      <Spacer />
+      <Button textStyle="omegaSemibold" as="p">
+        Book Now
+      </Button>
+    </Flex>
   </Box>
 );
 
