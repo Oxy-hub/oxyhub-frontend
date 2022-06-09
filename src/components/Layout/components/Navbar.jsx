c55ec6e7bd865f2c0c9b77c5ba97c8ef74fe1f8c
saby22 <sabyasachi.tffs@gmail.com>
2022-06-09 21:38:57 +0530

    Removed placeholder link

--- src/components/Layout/components/Navbar.jsx
+++ src/components/Layout/components/Navbar.jsx
@@ -43,10 +43,7 @@ const Navbar = () => (
           <Menu isLazy gutter={14} placement="top-end" autoSelect={false}>
             {/* Avatar src needs to be updated */}
             <MenuButton>
-              <Avatar
-                size="sm"
-                src="https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-              />
+              <Avatar size="sm" />
             </MenuButton>
 
             <MenuList>
