d3feadd027dddba1465d9cef3214c2cc72d49959
saby22 <sabyasachi.tffs@gmail.com>
2022-06-09 19:00:08 +0530

    Customized native Chakra UI Table component

--- src/theme/components/index.js
+++ src/theme/components/index.js
@@ -1,7 +1,8 @@
 import Menu from './Menu';
 import Link from './Link';
 import Modal from './Modal';
+import Table from './Table';
 
 export default {
-  components: { Menu, Link, Modal }
+  components: { Menu, Link, Modal, Table }
 };
