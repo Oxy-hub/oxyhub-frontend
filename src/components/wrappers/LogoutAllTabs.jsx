c0c4092b08592b7e15533c4fbc5079a3bc659497
saby22 <sabyasachi.tffs@gmail.com>
2022-06-13 17:27:12 +0530

    Removed cached queryClient data from the react-query

--- src/components/wrappers/LogoutAllTabs.jsx
+++ src/components/wrappers/LogoutAllTabs.jsx
@@ -1,18 +1,23 @@
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { useQueryClient } from 'react-query';
+
 import useBrodcastChannel from '../../hooks/useBrodcastChannel';
-import { resetAuth, unsetLoader } from '../../store/actions';
+import { resetAuth, unsetLoader, destroySession } from '../../store/actions';
 
 const LogoutAllTabs = ({ children }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   /** Reset auth if one tab logs out */
   useBrodcastChannel('auth', event => {
     if (event.data === 'logout') {
       dispatch(resetAuth());
+      queryClient.removeQueries();
       navigate('/');
       dispatch(unsetLoader());
+      dispatch(destroySession());
     }
   });
 
